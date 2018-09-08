8d1196b8d6f8454486815f82c5192207c517872a
zane <cn.zanelee@gmail.com>
2018-09-08 19:44:45 +0800

    [+] init

---
+++ component.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @author Zane(lzaneli) cn.zanelee@gmail.com
+ */
+
+class Component {
+    constructor(props) {
+        this.props = props || {};
+        this.state = this.state || {};
+    }
+
+    setState(partialState) {
+        scheduleUpdate(this, partialState);
+    }
+}
+
+function createInstance(fiber) {
+    const instance = new fiber.type(fiber.props);
+    instance.__fiber = fiber;
+    return instance;
+}
---
+++ element.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @author Zane(lzaneli) cn.zanelee@gmail.com
+ */
+
+const TEXT_ELEMENT = "TEXT ELEMENT";
+
+function createElement(type, config, ...args) {
+    const props = Object.assign({}, config);
+    const hasChildren = args.length > 0;
+    const rawChildren = hasChildren ? [].concat(...args) : [];
+    props.children = rawChildren
+        .filter(c => c != null && c !== false)
+        .map(c => c instanceof Object ? c : createTextElement(c));
+    return { type, props };
+}
+
+function createTextElement(value) {
+    return createElement(TEXT_ELEMENT, { nodeValue: value });
+}
